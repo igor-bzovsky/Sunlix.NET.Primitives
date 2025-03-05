4b3b548e6e2d9e60455cc86a295c87265e4c77de
igor-bzovsky <igor.bzovsky@gmail.com>
2025-03-05 22:45:52 +0200

    Multiple improvements and test coverage

--- src/Sunlix.NET.Primitives/Entity.cs
+++ src/Sunlix.NET.Primitives/Entity.cs
@@ -2,37 +2,57 @@
 
 namespace Sunlix.NET.Primitives
 {
-    public abstract class Entity<TId> : IEquatable<Entity<TId>> where TId : notnull
+    public abstract class Entity<TId> : IEquatable<Entity<TId>>, IComparable, IComparable<Entity<TId>>
+        where TId : IComparable<TId>
     {
-        public TId Id { get; }
+        public virtual TId? Id { get; protected set; }
 
-        protected Entity(TId id) => Id = id ?? throw new ArgumentNullException(nameof(id));
+        protected Entity() { }
+        protected Entity(TId id) => Id = id;
 
-        public bool Equals(Entity<TId>? other)
+        public virtual bool Equals(Entity<TId>? other)
         {
-            if (ReferenceEquals(null, other)) return false;
+            if (other is null) return false;
             if (ReferenceEquals(this, other)) return true;
-            return EqualsCore(other);
+            if (other.GetType() != GetType()) return false;
+            return SatisfiesIdentifierEquality(other);
         }
 
         public override bool Equals(object? obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return EqualsCore((Entity<TId>)obj);
-        }
+            => obj is Entity<TId> other && Equals(other);
 
-        public override int GetHashCode() => Id.GetHashCode();
+        public override int GetHashCode() => HashCode.Combine(GetType(), Id?.GetHashCode() ?? 0);
 
         public static bool operator ==(Entity<TId>? left, Entity<TId>? right)
+            => left is null ? right is null : left.Equals(right);
+
+        public static bool operator !=(Entity<TId>? left, Entity<TId>? right)
+            => !(left == right);
+
+        public virtual int CompareTo(Entity<TId>? other)
         {
-            if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null)) return false;
-            return ReferenceEquals(left, null) || left.Equals(right);
+            if (other is null) return 1;
+            if (Id is null && other.Id is null) return 0;
+            if (Id is null) return -1;
+            if (other.Id is null) return 1;
+
+            return Id.CompareTo(other.Id);
         }
 
-        public static bool operator !=(Entity<TId>? left, Entity<TId>? right) => !(left == right);
+        public virtual int CompareTo(object? obj)
+        {
+            if (obj is null) return 1;
+            if (obj is not Entity<TId> other)
+                throw new ArgumentException("Object type mismatch. Ensure the correct type is used.", nameof(obj));
 
-        private bool EqualsCore(Entity<TId> other) => Id.Equals(other.Id);
+            return CompareTo(other);
+        }
+
+        private bool SatisfiesIdentifierEquality(Entity<TId> other)
+        {
+            if (Id is null || Id.Equals(default)) return false;
+            if (other.Id is null || other.Id.Equals(default)) return false;
+            return Id.Equals(other.Id);
+        }
     }
 }
